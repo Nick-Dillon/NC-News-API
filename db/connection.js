42a2eb67bca01ba212ccd209e2414f708a831dd4
Nick-Dillon <ndillon18@hotmail.co.uk>
2019-04-01 10:55:46 +0100

    changed connection.js to be process.env.DATABASE_URL

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const ENV = process.env.NODE_ENV || 'development';
-const config = ENV === 'production' ? { client: 'pg', connection: process.env.DB_URL } : require('../knexfile');
+const config = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile');
 
 console.log(config, 'config in connection.js')
 module.exports = require('knex')(config);
