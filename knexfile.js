91a01706f82fd34f29c9934395fa7db6c3e9f585
Nick-Dillon <ndillon18@hotmail.co.uk>
2019-04-01 09:01:06 +0100

    trying to recognise knexfile

---
+++ knexfile.js
@@ -0,0 +1,37 @@
+const ENV = process.env.NODE_ENV || 'development';
+
+const baseConfig = {
+  client: 'pg',
+  migrations: {
+    directory: './db/migrations',
+  },
+  seeds: {
+    directory: './db/seeds',
+  },
+};
+
+const dbConfig = {
+  development: {
+    connection: {
+      database: 'nc_news',
+      username: 'nicholas',
+      password: 'rated',
+    },
+  },
+  test: {
+    connection: {
+      database: 'nc_news_test',
+      username: 'nicholas',
+      password: 'rated',
+    },
+  production: {
+    connection: {
+      database: 'nc_news',
+      username: 'nicholas',
+      password: 'rated'
+    }
+  }
+  },
+};
+
+module.exports = { ...baseConfig, ...dbConfig[ENV] };
