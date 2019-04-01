8cf1a9bba2ca35b8a258a38b26be3d6234fa993a
Nick-Dillon <ndillon18@hotmail.co.uk>
2019-04-01 08:49:58 +0100

    config file deleted

--- config.js
+++
@@ -1,6 +0,0 @@
-const iam_apikey = "";  // api key given by heroku
-// const url = "https://ncnews-api.herokuapp.com/" // url given by heroku <--- this one?
-const url = "https://git.heroku.com/ncnews-api.git" // url given by heroku
-
-export default iam_apikey;
-export default url;
